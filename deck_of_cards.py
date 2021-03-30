36cb7caab08fd6c5b7cfef3639ec568bd92a61bb
DeadSage <olegnaruto1012001@gmail.com>
2021-03-30 08:05:39 +0300

    BJ

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,26 +1,6 @@
 import random
 import json
-
-
-class Dealer:
-    """
-    инициализация карт диллера
-    """
-
-    def __init__(self):
-        self.dealer_hand = []
-
-    def draw(self, deck, num=2):
-        """
-        взятие n карт из колоды до тех пор, пока они там есть
-        """
-        for _ in range(num):
-            card = deck.deal()
-            if card:
-                self.dealer_hand.append(card)
-            else:
-                return False
-        return True
+import time
 
 
 class Card:
@@ -112,6 +92,7 @@ class Player:
         self.hand = []
         self.bet = 0
         self.sum_p = 0
+        self.money = 100
 
     def sum_points(self):
         self.sum_p = sum([card.points for card in self.hand])
@@ -126,6 +107,7 @@ class Player:
             val = int(input('Make your bet: '))
             if min_bet < val < max_bet:
                 self.bet = val
+                self.money -= self.bet
             print(f'Player give {self.bet}')
             break
 
@@ -156,21 +138,87 @@ class Player:
         """
         return self.hand.pop()
 
+    def ask_card(self):
+        if self.sum_p == 21:
+            return False
+        choice = input('Want more card(yes/no)')
+        if choice == 'yes':
+            return True
+        else:
+            return False
+
+
+# class Dealer(Player):
+#     """
+#     инициализация карт диллера
+#     """
+#
+#     def __init__(self):
+#         self.hand = []
+#         self.max_points = 17
+#
+#     def sum_points(self):
+#         self.sum_p = sum([card.points for card in self.hand])
+#
+#     def draw(self, deck, num=2):
+#         """
+#         взятие n карт из колоды до тех пор, пока они там есть
+#         """
+#         for _ in range(num):
+#             card = deck.deal()
+#             if card:
+#                 self.hand.append(card)
+#             else:
+#                 return False
+#         return True
+#
+#     def ask_card(self):
+#         print('Dealer thinking...')
+#         if self.sum_p < self.max_points:
+#             return True
+#         else:
+#             return False
+#
+#     def change_bet(self):
+#         max_bet = 50
+#         min_bet = 0
+#         self.bet = random.randint(min_bet, max_bet)
+
 
 class Bot(Player):
     """
     исскуственный интеллект
     """
-    def __init__(self, position):
+    def __init__(self):
         self.hand = []
-        self.position = position
         self.bet = 0
+        # self.max_points = random.randint(17, 20)
+        self.max_points = 17
+        self.money = 100
 
     def change_bet(self):
         max_bet = 50
         min_bet = 0
         self.bet = random.randint(min_bet, max_bet)
+        self.money -= self.bet
+        print('Bot preparing to make a bet...')
+        time.sleep(1)
         print(f'Bot give {self.bet}')
+
+    def showHand(self):
+        """
+        отображение руки бота
+        """
+        for card in self.hand:
+            print(f'{card.show()} ', end='')
+        print(f'\nTotal points: {self.sum_p}')
+
+    def ask_card(self):
+        print('Bot thinking...')
+        if self.sum_p < self.max_points:
+            return True
+        else:
+            return False
 
 
 class Game:
@@ -198,13 +246,19 @@ class Game:
         """
         садимся за столик
         """
-        p_count = int(input('Enter count of players: '))
+        while True:
+            p_count = int(input('Enter count of players: '))
+            if p_count <= 4:
+                break
         self.a_p_count = p_count + 1
+        pos = 1
         for i in range(p_count):
-            b = Bot(position=i)
+            b = Bot()
             # print(f'{b} is created')
             self.players.append(b)
+            pos += 1
         self.players.append(Player())
+        print(f'Your position is {pos}')
 
     def bet(self):
         """
@@ -212,6 +266,42 @@ class Game:
         """
         for player in self.players:
             player.change_bet()
+
+    def check(self, player):
+        if player.sum_p >= 21:
+            return True
+        else:
+            return False
+
+    def remove_player(self, player):
+        player.showHand()
+        if isinstance(player, Player):
+            print('You are loser!')
+        elif isinstance(player, Bot):
+            print(player, 'are fall!')
+        self.players.remove(player)
+
+    def check_wins(self):
+        for player in self.players:
+            player.money += player.bet * 2
+        if isinstance(player, Bot):
+            print('Bot wins!')
+        if isinstance(player, Player):
+            print('You are win!')
+
+    def ask_card(self):
+        for player in self.players:
+            while player.ask_card():
+                time.sleep(3)
+                player.draw(deck, 1)
+                print('============================================================')
+                player.showHand()
+                print('============================================================')
+                stop = self.check(player)
+                if stop:
+                    if player.sum_p > 21 or isinstance(player, Player):
+                        self.remove_player(player)
+                    break
 
     def game_start(self):
         """
@@ -222,15 +312,18 @@ class Game:
         self.gen_table()
         self.bet()
         self.create_hand()
+        self.ask_card()
+        self.check(player)
 
     def create_hand(self):
         for player in self.players:
             Player.draw(player, deck, 2)
+            print('============================================================')
             print(f'{player.showHand()}')
 
 
 deck = Deck()
-dealer = Dealer()
+bot = Bot()
 player = Player()
 game = Game()
 print(game.game_start())
